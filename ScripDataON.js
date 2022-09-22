47f5ba9775c392704c9f05ccfa0069ed06b4f50b
Sheila Driele <sheiladriele@hotmail.com>
2022-09-21 23:05:56 -0300

    Scrit Data On do VS

---
+++ ScripDataON.js
@@ -0,0 +1,103 @@
+
+<strong>.:Data On:.</strong><br />2026 - Setembro<br /> <script language="JavaScript" type="text/javascript">
+
+//Altera o dia ON, baseado no dia OFF
+
+dON = new Date();
+switch (dON.getDate()) {
+case 1:
+document.write ("Dia ON 1 2 3<br>*Lua Nova*");
+break;
+case 2:
+document.write ("Dia ON 4 5 6<br>**Lua Nova**");
+break;
+case 3:
+document.write ("Dia ON 7 8 9<br>*Lua Crescente*");
+break;
+case 4:
+document.write ("Dia ON 10 11 12<br>**Lua Crescente**");
+break;
+case 5:
+document.write ("Dia ON 13 14 15<br>***Lua Crescente***");
+break;
+case 6:
+document.write ("Dia ON 16 17 18<br>*Lua Cheia*");
+break;
+case 7:
+document.write ("Dia ON 19 20 21<br>**Lua Cheia**");
+break;
+case 8:
+document.write ("Dia ON 22 23 24<br>*Lua Minguante*");
+break;
+case 9:
+document.write ("Dia ON 25 26 27<br>**Lua Minguante**");
+break;
+case 10:
+document.write ("Dia ON 28 29 30<br>***Lua Minguante***");
+break;
+case 11:
+document.write ("Dia ON 1 2 3<br>*Lua Nova*");
+break;
+case 12:
+document.write ("Dia ON 4 5 6<br>**Lua Nova**");
+break;
+case 13:
+document.write ("Dia ON 7 8 9<br>*Lua Crescente*");
+break;
+case 14:
+document.write ("Dia ON 10 11 12<br>**Lua Crescente**");
+break;
+case 15:
+document.write ("Dia ON 13 14 15<br>***Lua Crescente***");
+break;
+case 16:
+document.write ("Dia ON 16 17 18<br>*Lua Cheia*");
+break;
+case 17:
+document.write ("Dia ON 19 20 21<br>*Lua Minguante*");
+break;
+case 18:
+document.write ("Dia ON 22 23 24<br>**Lua Minguante**");
+break;
+case 19:
+document.write ("Dia ON 25 26 27<br>***Lua Minguante***");
+break;
+case 20:
+document.write ("Dia ON 28 29 30<br>*Lua Nova*");
+break;
+case 21:
+document.write ("Dia ON 1,2,3<br>**Lua Nova**");
+break;
+case 22:
+document.write ("Dia ON 4 5 6<br>*Lua Crescente*");
+break;
+case 23:
+document.write ("Dia ON 7 8 9<br>**Lua Crescente**");
+break;
+case 24:
+document.write ("Dia ON 10 11 12<br>***Lua Crescente***");
+break;
+case 25:
+document.write ("Dia ON 13 14 15<br>*Lua Cheia*");
+break;
+case 26:
+document.write ("Dia ON 16,17,18<br>**Lua Cheia**");
+break;
+case 27:
+document.write ("Dia ON 19 20 21<br>*Lua Minguante*");
+break;
+case 28:
+document.write ("Dia ON 22 23 24<br>**Lua Minguante**");
+break;
+case 29:
+document.write ("Dia ON 25 26 27<br>***Lua Minguante***");
+break;
+case 30:
+document.write ("Dia ON 28 29 30<br>***Lua Minguante***");
+break;
+default:
+// se não for nenhum dos cases acima, é 31 então:
+document.write ("Dia ON 29 30 31<br>*Lua Nova*");
+}
+</script>
+
